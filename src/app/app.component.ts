1a1291351223745ee8e1ef5aadbb4a35d8999644
Greg Manda <grzegorz.manda@gmail.com>
2019-05-17 10:36:17 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-demo';
+  myForm: FormGroup;
+
+  items$: Observable<{ task: string }[]>;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      task: ['']
+    });
+  }
+
+  ngOnInit() { }
+
+  addTask() {
+    // dispatch action here
+    console.log(`[addTask]`);
+  }
+
 }
